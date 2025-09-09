64e1b405913ea7b73957fd13a91771753c63b498
Rahul Hegde <rgh9883@rit.edu>
2025-09-09 18:47:23 -0400

    added relative line numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,9 +3,12 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
+
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
